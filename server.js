d9090f90c67055a5b1bb0cb320f79ce49938456b
Max Serhieiev <serhieiev@users.noreply.github.com>
2018-06-08 14:46:46 +0300

    Update server.js
    Added rendering of env. variables for testing

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  let envVars = JSON.stringify(process.env, null, 4);
+  res.render('index', {weather: envVars, error: null});
 })
 
 app.post('/', function (req, res) {
